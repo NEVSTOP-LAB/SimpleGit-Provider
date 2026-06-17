6f36dddaaba54aa6891d68f20a6b3c58595ef067
nevstop <nevstop@NEVSTOP-LAB>
2026-06-17 09:42:22 +0800

    更新 un-mklink.cmd 脚本，移除不再使用的 LVAPT 相关命令，并保留历史记录注释

--- un-mklink.cmd
+++ un-mklink.cmd
@@ -1,14 +1,14 @@
-rmdir "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\LVAPT"
-REM mklink /J "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\SecondaryProviderExample2" "%~dp0SecondaryProviderExample2"
-REM mklink /J "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\PrimaryProviderExample2" "%~dp0PrimaryProviderExample2"
-REM mklink /J "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\DummyPrimaryProvider" "%~dp0DummyPrimaryProvider"
+rmdir "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\SimpleGit"
+del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\SimpleGit.ini"
 
-del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_System.ini"
-del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Document.ini"
-del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_VirtualFolder.ini"
-del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Project.ini"
-del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Class.ini"
-del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Git.ini"
+REM === 以下为历史记录（原 LVAPT 相关撤销操作，已不再使用） ===
+REM rmdir "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\LVAPT"
+REM del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_System.ini"
+REM del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Document.ini"
+REM del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_VirtualFolder.ini"
+REM del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Project.ini"
+REM del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Class.ini"
+REM del "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\GProviders\LVAPT_Git.ini"
+REM rmdir "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\Icons\fatcow-hosting"
 
-rmdir "C:\Program Files (x86)\National Instruments\LabVIEW 2017\resource\Framework\Providers\Icons\fatcow-hosting"
 pause
